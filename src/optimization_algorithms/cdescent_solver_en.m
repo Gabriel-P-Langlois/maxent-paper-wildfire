9b7d1121e6260398bae2e3f09ae5784ae6efdb1e
Gabriel P. Langlois <Gabriel_provencher_langlois@alumni.brown.edu>
2023-11-20 15:20:39 -0500

    Finished the coordinate descent and classic FISTA algorithms. These were incorporated in the script file, which is now renamed script_maxent_elastic_net.
    I also added several options to the script, including a save option and display option.

--- src/optimization_algorithms/cdescent_solver_en.m
+++ src/optimization_algorithms/cdescent_solver_en.m
@@ -1,8 +1,85 @@
 %% Description
-%
+% Nonlinear PDHG method for solving Maxent with lambda*norm(\cdot)_{1}
+% Input variables:
+%   w_in: m x 1 vector -- Weights of the gibbs distribution.
+%   p_in: n x 1 vector -- Parameterization of the gibbs probability
+%   distribution, where p(j) = pprior(j)e^{u(j)-C}.
+%   t: Positive number -- Hyperparameter.
+%   alpha: parameter for the convexity between the l1 norm and l22 norms.
+%   A: n x m matrix -- Matrix of features (m) for each grid point (n).
+%   Ed: m-dimensional vector -- Observed features of presence-only data. 
+%   max_iter: Positive integer -- Maximum number of iterations.
+%   tol:    Small number -- used for the convergence criterion
+
+% Output:
+%   sol_w: m x 1 column vector -- dual solution
+%   sol_p: n x 1 column vector -- primal solution
+
+function [w_out,p_out,num_iters] = cdescent_solver_en(w_in,pprior,...
+    t,alpha,A,Ed,max_iter,tol)
+% Auxiliary variables -- For the algorithm
+tmp1 = ((pprior'*A)') - Ed;
+tmp2 = A*w_in;
+wplus = w_in;
+
+% Main algorithm
+num_iters = 0; 
+flag_convergence = true;
+d = zeros(length(w_in),1);
 
 
-%% Function
-function [w_out,p_out] = cdescent_solver_en(w_in,p_in,t,alpha)
 
+% In the following, we need to compute estimate the stepsize of the 
+% coordinate descent algorithm. This stepsize is derived from the analysis
+% on pages 305-306 of Mohri et al. (2019) ML textbook.
+
+while (flag_convergence)
+    % Update counter
+    num_iters = num_iters + 1;
+    
+    % Gradient of the smooth term of the object function
+    tmp3 = tmp1 + (1-alpha)*t*w_in;
+
+    % Compute descent directions
+    for i=1:1:length(w_in)
+       if(w_in(i)~=0)
+           d(i) = (alpha*t)*sign(w_in(i)) + tmp3(i);
+       elseif(abs(tmp3(i)) <= (alpha*t))
+           d(i) = 0;
+       else
+           d(i) = -(alpha*t)*sign(w_in(i)) + tmp3(i);
+       end
+    end
+    [~,j_ind] = max(abs(d));
+    
+    % Approximate steepest descent
+    step_cond = w_in(j_ind) - tmp1(j_ind);
+
+    if(abs(step_cond) <= (alpha*t))
+        eta = -w_in(j_ind);
+    elseif(step_cond > (alpha*t))
+        eta = -((alpha*t)+tmp3(j_ind))/(1+t*(1-alpha));
+    else
+        eta = ((alpha*t)-tmp3(j_ind))/(1+t*(1-alpha));
+    end
+    
+    % Update dual variable + scalar product
+    wplus(j_ind) = w_in(j_ind) + eta;
+    tmp2 = tmp2 + eta*A(:,j_ind);
+
+    % Update the probability and the difference in expectations
+    pplus = exp(tmp2-max(tmp2)); norm_sum = sum(pplus);
+    tmp1 = ((pplus'*A)')/norm_sum - Ed;
+    
+    % Increment dual variable
+    w_in = wplus;
+
+    % Convergence check -- We use the optimality condition on the l1 normd
+    flag_convergence = ~(((num_iters >= 40) && (norm((1-alpha)*t*wplus + tmp1,inf) <= ...
+            alpha*t*(1 + tol))) || (num_iters >= max_iter));
+end
+
+% Final solutions
+p_out = pplus/norm_sum;
+w_out = wplus;
 end
--- src/optimization_algorithms/fista_solver_en.m
+++ src/optimization_algorithms/fista_solver_en.m
@@ -4,8 +4,51 @@
 % Maxent problem with the FISTA algorithm.
 
 
-function [w_out,p_out,z_out] = fista_solver_en(w_in,p_in,z_in,t,alpha,...
-    norm_fac)
+function [w_out,p_out,num_iters] = fista_solver_en(w_in,pprior,lambda,...
+    alpha,A,tau,mu,q,Ed,max_iter,tol)
 
+    % Auxiliary variables, factors and variables
+    num_iters = 0; 
+    flag_convergence = true(1);
 
+    % Initial quantities
+    tk = 1;
+    w_minus = w_in; wplus = w_in;
+
+    while (flag_convergence)
+        % Update counter
+        num_iters = num_iters + 1;
+
+        % Stepsies
+        tkplus = 0.5*(1-q*(tk^2) + sqrt((1-q*(tk^2))^2 + 4*(tk^2)));
+        beta = ((tk-1)/tkplus)*(1+mu*tau*(1-tkplus));
+
+        % Compute next extragradient step
+        yk = w_in + beta*(w_in-w_minus);
+
+        % Evaluate probability from the extragradient step
+        u = A*yk;
+        pplus = pprior.*exp(u-max(u));
+        pplus = pplus/sum(pplus);
+
+        % Evaluate argument of the elastic net proximal operator
+        tmp = (pplus.'*A).' - Ed;
+
+        % Compute the EN proximal operator
+        wplus = proximal_operator_en(yk - tau*(tmp),lambda*tau,alpha);
+
+        % Convergence check -- Check that the optimality condition of the
+        % elastic net penalty is satisfied after enough iterations
+        flag_convergence = ~(((num_iters >= 40) && (norm((1-alpha)*lambda*wplus + tmp,inf) <= ...
+            alpha*lambda*(1 + tol))) || (num_iters >= max_iter));
+
+        % Increment
+        tk = tkplus;
+        w_minus = w_in; w_in = wplus;
+
+    end
+
+    % Final solutions
+    p_out = pplus;
+    w_out = wplus;
 end
